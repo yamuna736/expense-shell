d42f87f30d8460c7fe163ff423827ff2560d656a
“yamuna <grvyamunasree1320@gmail.com>
2024-09-09 00:19:43 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOGS_FOLDER="/var/logs/expense"
+LOGS_FOLDER="/var/logs/expense-shell"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
