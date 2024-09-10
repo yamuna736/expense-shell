c7ddf327dca3100d8730fbda2caa33cf3326c42e
“yamuna <grvyamunasree1320@gmail.com>
2024-09-10 13:43:32 +0530

    shell

--- backend.sh
+++ backend.sh
@@ -79,7 +79,7 @@ fi
 
         mysql -h mysql.batch1320.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
         # sql schema will be in backend : cat /app/schema/backend.sql
-        VALIDATE $? " shchema loading.."
+        VALIDATE $? "shchema loading.."
 
         systemctl daemon-reload &>>$LOG_FILE
         VALIDATE $? "Daemon relaod"
